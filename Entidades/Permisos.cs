c182a4c083dc33ea282162bd33a18a7739d28390
Felix <felix_reynoso1@ucne.edu.do>
2021-10-11 00:00:04 -0400

    Agregando llave primaria a Permisos

--- Entidades/Permisos.cs
+++ Entidades/Permisos.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace RolesConPermisos.Entidades
 {
     public class Permisos
     {
+        [Key]
         public int PermisoID { get; set; }
         public string Nombre { get; set; }
         public string Descripcion { get; set; }

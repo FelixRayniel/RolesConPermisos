145e230bc6fc1741204d5a1b9a53aa9b0f2fb8e9
Felix <felix_reynoso1@ucne.edu.do>
2021-10-16 00:09:56 -0400

    Agregando PermisosBLL.

---
+++ BLL/PermisosBLL.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RolesConPermisos.DAL;
+using RolesConPermisos.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RolesConPermisos.BLL
+{
+   public class PermisosBLL
+    {
+        public static Permisos Buscar(int id)
+        {
+            Contexto contexto = new Contexto();
+            Permisos permiso;
+
+            try
+            {
+                permiso = contexto.Permisos.Find(id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return permiso;
+        }
+
+        /// <summary>
+        /// Permite obtener una lista filtrada por un criterio de busqueda
+        /// </summary>
+        /// <param name="criterio">La expresión que define el criterio de busqueda</param>
+        /// <returns></returns
+        public static List<Permisos> GetList(Expression<Func<Permisos, bool>> criterio)
+        {
+            List<Permisos> lista = new List<Permisos>();
+            Contexto contexto = new Contexto();
+            try
+            {
+                lista = contexto.Permisos.Where(criterio).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return lista;
+        }
+
+        public static List<Permisos> GetPermisos()
+        {
+            List<Permisos> lista = new List<Permisos>();
+            Contexto contexto = new Contexto();
+            try
+            {
+                lista = contexto.Permisos.ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return lista;
+        }
+    }
+}

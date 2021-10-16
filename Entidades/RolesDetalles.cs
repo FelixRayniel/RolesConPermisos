5442e65206456f243751896b58a732f954ebc84a
Felix <felix_reynoso1@ucne.edu.do>
2021-10-15 23:25:59 -0400

    Agregando datos faltantes a las entidades.

--- Entidades/RolesDetalles.cs
+++ Entidades/RolesDetalles.cs
@@ -14,6 +14,8 @@ namespace RolesConPermisos.Entidades
         public int RolID { get; set; }
         public int PermisoId { get; set; }
         public bool esAsignado { get; set; }
+        public string NombreAsignado { get; set; }
+
 
     }
 }

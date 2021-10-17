88627d293ccaacd44102fee3b679a495b1859667
Felix <felix_reynoso1@ucne.edu.do>
2021-10-17 17:46:18 -0400

    Agregando el Registro.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RolesConPermisos.UI.Registros;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,12 @@ namespace RolesConPermisos
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            rRoles rr = new rRoles();
+            rr.Show();
         }
     }
 }
